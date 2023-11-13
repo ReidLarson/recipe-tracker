928cf64103ad71ae0ff3346b516ae0ff4198af84
Reid Larson <reid.d.larson@gmail.com>
2023-11-12 21:52:38 -0500

    Use init only properties

--- src/RecipeTracker.Core/Commands/CreateRecipeCommand.cs
+++ src/RecipeTracker.Core/Commands/CreateRecipeCommand.cs
@@ -2,6 +2,6 @@ namespace RecipeTracker.Core.Commands;
 
 public class CreateRecipeCommand
 {
-    public required string Name { get; set; }
-    public required string Description { get; set; }
+    public required string Name { get; init; }
+    public required string Description { get; init; }
 }
